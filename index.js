0ae0f680cbf0348df5686faf1bd5d8bb044068f5
wukiwuk <shaquille.fazhira@gmail.com>
2024-07-30 20:16:21 +0700

    nothing works

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-let display = document.getElementById("display")
+let display = document.getElementById("display");
+
+function appendNumber(number) {
+  if (display.innerText === "0") {
+    display.innerText = number;
+  } else {
+    display.innerText += number;
+  }
+}
+
+function appendOperator(operator) {
+  const lastChar = display.innerText.slice(-1);
+  if (["+", "-", "*", "/"].includes(lastChar)) {
+    display.innerText = display.innerText.slice(0, -1) + operator;
+  } else {
+    display.innerText += operator;
+  }
+}
+
+function clearDisplay() {
+  display.innerText = "0";
+}
+
+function deleteLast() {
+  if (display.innerText.length > 1) {
+    display.innerText = display.innerText.slice(0, -1);
+  } else {
+    display.innerText = "0";
+  }
+}
+
+function calculate() {
+  try {
+    display.innerText = eval(display.innerText);
+  } catch (e) {
+    display.innerText = "Error";
+  }
+}
